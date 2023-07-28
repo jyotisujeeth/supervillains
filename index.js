64a7b3fd133fb221a9e324bf3feb8b4cae36b74b
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-07-28 13:26:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const supervillains = require("supervillains");
 
 console.log(supervillains);
+const supervillains = require('supervillains');
+
+// supervillains.all;
+// //=> ['Abattoir', 'Able Crown', …]
+
+// supervillains.random();
+// //=> 'Mud Pack'
